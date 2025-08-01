8ddf5d45ab75d252bf8afda6ca47dc73b955c7e4
Ouse <2216528769@qq.com>
2025-08-01 23:33:51 +0800

    预览完成！

---
+++ SyncArea/SyncArea/wwwroot/downloadService.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = function (url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = url.split('/').pop() || 'download.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
